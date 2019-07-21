947c6d66f1abccede66aeeac53ac198249e3e96f
Ankita <kumariankita.gaurav@cgi.com>
2019-07-22 00:00:47 +0530

    "modified"

--- src/main/java/com/stackroute/printpages/MainClass.java
+++ src/main/java/com/stackroute/printpages/MainClass.java
@@ -1,14 +1,15 @@
 package com.stackroute.printpages;
 
 public class MainClass {
-    Printer prntr = new Printer("mukul",50,20,true);
+    public static void main(String[] args) {
+        Printer prntr = new Printer("mukul", 50, 20, true);
 
         System.out.println("Page count in initial = " + prntr.getPrintedPages());
-    int pagesPrinted = prntr.print(5);
+        int pagesPrinted = prntr.print(5);
         System.out.println("Pages printed was " + pagesPrinted + ". New total print count = " + prntr.getPrintedPages());
-    pagesPrinted = printer.print(3);
+        pagesPrinted = printer.print(3);
         System.out.println("Pages printed was " + pagesPrinted + ". New total print count = " + prntr.getPrintedPages());
 
 
-
+    }
 }
